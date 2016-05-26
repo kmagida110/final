7a2e53e6e6e00a0d27485a5cf7be9655333ac6fa
Kyle Magida <kmagida@gmail.com>
2016-05-26 16:06:46 -0500

    main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
 
 	def index
 		@landmarks = Landmark.all.limit(6)
-		@cur_user = User.find_by(id: session[:user_id])
+		#@cur_user = User.find_by(id: session[:user_id])
 		@routes = Route.all.limit(5)
 		# Show landmarks they haven't considered
 		if @cur_user
